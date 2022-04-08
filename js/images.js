4b5ba05af5632489e9cf3b9c30c41a80008a090c
Emil Zackrisson <emil.zackrisson@gmail.com>
2022-04-08 10:20:13 +0200

    hejsan

---
+++ js/images.js
@@ -0,0 +1,36 @@
+function hover1() {
+    console.log("HOVER!!!")
+    document.getElementById("img1").style.zIndex = "1";
+    document.getElementById("img1").style.width = "150%";
+
+}
+
+function noHover1() {
+    console.log("NO HOVER")
+    document.getElementById("img1").style.zIndex = "0";
+    document.getElementById("img1").style.width = "100%";
+}
+
+function hover2() {
+    console.log("HOVER!!!")
+    document.getElementById("img1").style.zIndex = "1";
+    document.getElementById("img2").style.width = "150%";
+}
+
+function noHover2() {
+    console.log("NO HOVER")
+    document.getElementById("img1").style.zIndex = "0";
+    document.getElementById("img2").style.width = "100%";
+}
+
+function hover3() {
+    console.log("HOVER!!!")
+    document.getElementById("img1").style.zIndex = "1";
+    document.getElementById("img3").style.width = "150%";
+}
+
+function noHover3() {
+    console.log("NO HOVER")
+    document.getElementById("img1").style.zIndex = "0";
+    document.getElementById("img3").style.width = "100%";
+}
